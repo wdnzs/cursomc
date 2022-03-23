967b637e6c2d217fbbb58f8ef008683b41f52bbb
Wilson Diniz Silva <diniztte@gmail.com>
2022-03-22 22:51:58 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -11,19 +11,18 @@ import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService service;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		
+
 		Categoria obj = service.buscar(id);
-		
 		return ResponseEntity.ok().body(obj);
-		
+
 	}
 
 }
